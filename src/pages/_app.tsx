0ac9e7ee13c76ef8c93814dd89363290b22a8880
ogihara-ryo <ryo@ryukalice.com>
2020-05-20 18:31:36 +0900

    Add App component for layout

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import { AppProps } from "next/app";
+
+const Ryukalice = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default Ryukalice;
