7cef9f2ab22374598bc7e389900d59f0a0fc0211
ogihara-ryo <ryo@ryukalice.com>
2020-05-22 22:53:08 +0900

    Apply styled-components to Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,18 @@
-const Header = () => <h1>Header</h1>;
+import styled from "styled-components";
+import Link from "next/link";
+
+const Logo = styled.h1`
+  font-size: 24px;
+`;
+
+const Header = () => (
+  <>
+    <Link href="/">
+      <a>
+        <Logo>Ryukalice</Logo>
+      </a>
+    </Link>
+  </>
+);
 
 export default Header;
