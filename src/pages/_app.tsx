1864f43efb5157bd998fb732b8217df866715de5
Ogihara Ryo <ryo@ryukalice.com>
2020-06-09 11:37:06 +0900

    Create GlobalStyle for hover link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from "next/app";
-import Header from "components/layout/Header";
+import { GlobalStyle } from "styles/GlobalStyle";
+import { Header } from "components/layout/Header";
 import Main from "components/layout/Main";
 import Footer from "components/layout/Footer";
 
 const Ryukalice = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Main>
         <Component {...pageProps} />
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  a:hover {
+    color: #f5deb3;
+    transition: all 0.2s ease;
+  }
+`;
