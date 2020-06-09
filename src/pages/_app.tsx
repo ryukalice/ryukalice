79415fe7f7789953d897b0010b98cb4d24f70f60
Ogihara Ryo <ryo@ryukalice.com>
2020-06-09 12:28:30 +0900

    Styling Footer component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from "next/app";
 import { GlobalStyle } from "styles/GlobalStyle";
 import { Header } from "components/layout/Header";
 import { Main } from "components/layout/Main";
-import Footer from "components/layout/Footer";
+import { Footer } from "components/layout/Footer";
 
 const Ryukalice = ({ Component, pageProps }: AppProps) => {
   return (
