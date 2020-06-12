a4af2c88f0afc57ceb1cfd9635ba94040be89531
Ogihara Ryo <ryo@ryukalice.com>
2020-06-12 13:04:52 +0900

    Add meta elements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,9 +15,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
+        <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://ryukalice.com/" />
+          <meta
+            property="og:image"
+            content="https://ryukalice.com/images/alice.png"
+          />
+          <meta property="og:site_name" content="Ryukalice" />
+          <meta name="twitter:card" content="summary_large_image" />
 
           <link
             rel="stylesheet"
